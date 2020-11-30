75ec7da793ea6dadd05268b2a9308656b819f175
milkpool <103304042@nccu.edu.tw>
2020-11-30 22:52:24 +0800

    Find Music

---
+++ config.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var firebaseConfig = {
+	apiKey: "AIzaSyCgsyxI1Rq9XB1me87xjQEkoNyRq9ESGLM",
+	authDomain: "dsci-551-findmusictrend.firebaseapp.com",
+	databaseURL: "https://dsci-551-findmusictrend.firebaseio.com",
+	projectId: "dsci-551-findmusictrend",
+	storageBucket: "dsci-551-findmusictrend.appspot.com",
+	messagingSenderId: "813537591007",
+	appId: "1:813537591007:web:2ce56aa2b91e5113c2336a",
+	measurementId: "G-F7LSNX7DXR"
+};
+firebase.initializeApp(firebaseConfig);
